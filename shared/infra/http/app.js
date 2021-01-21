cb372fbb1d1aab1e81bb876638b600fe02fa944a
ibnbasri <epibn80@gmail.com>
2021-01-21 00:47:46 -0800

    upload

--- shared/infra/http/app.js
+++ shared/infra/http/app.js
@@ -6,11 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var router_1 = require("../../../modules/users/infra/http/router");
+var port = process.env.PORT || 3000;
 var app = express_1.default();
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use('/user', router_1.userRouter);
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("Application Running ON 3000");
 });
 //# sourceMappingURL=app.js.map
